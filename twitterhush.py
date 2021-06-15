66f56f1d9f8c0404b222407a3de2c107e5961054
Sota4077 <76750811+Sota4077@users.noreply.github.com>
2021-06-15 14:54:00 -0500

    Update twitterhush.py

--- twitterhush.py
+++ twitterhush.py
@@ -2,3 +2,11 @@
 
 #You will need to install the following packages if they are not already installed.
 import Tweepy
+
+#The following are the Twitter API credentials you would need to get from your Twitter account.  They are keps in a separate folder that is not updated to this repository for obvious security reasons.
+from credentials import consumer_key, consumer_secret, access_token, access_token_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
