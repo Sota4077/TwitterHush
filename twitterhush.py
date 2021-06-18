72f7b974cc521eec3a3057a563b7f6a7a72d4151
Sota4077 <dollada06@outlook.com>
2021-06-18 12:23:23 -0500

    testing gitignore

--- twitterhush.py
+++ twitterhush.py
@@ -1,3 +1,5 @@
 import sys
-sys.path.insert(0,'C:/Users/dolla/OneDrive/Python/Credentials')
-from credentials import * #imports the variabls consumer_key, consumer_secret, access_token & access_token_secret
+sys.path.insert(0,'Credentials/') #This needs to be updated to reflect the location of your Twitter API credentials on your machine.
+from credentials import * #imports the variabls consumer_key, consumer_secret, access_token & access_token_secret.
+
+print(consumer_key)
