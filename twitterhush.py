af831abf750184bb8d21895c3e48700e6d30ebc3
Adam <dollada06@outlook.com>
2021-06-22 22:37:02 -0500

    Updated Code with keyword search

--- twitterhush.py
+++ twitterhush.py
@@ -1,5 +1,32 @@
-import sys
-sys.path.insert(0,'Credentials/') #This needs to be updated to reflect the location of your Twitter API credentials on your machine.
-from credentials import * #imports the variabls consumer_key, consumer_secret, access_token & access_token_secret.
+from __future__ import unicode_literals
 
-print(consumer_key)
+import sys, logging, tweepy
+
+from credentials import * #imports the variabls consumer_key, consumer_secret, access_token & access_token_secret.
+logging.warning("Assembling keywords for keyword variable...")
+
+with open('keywords.txt') as f:
+    keywords = [line.rstrip() for line in f]
+logging.warning(keywords)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+
+class MyStreamListener(tweepy.StreamListener):
+    
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_error disconnects the stream
+            return False
+    def on_status(self, status):
+        print(status.user.screen_name)
+        print(status.text)
+        print("--------------------------------------")
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)\
+
+myStream.filter(track=keywords, is_async=True)
